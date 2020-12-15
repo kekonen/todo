f31ec869b4b98c70d80682ab8fc8b67fae7d7bcd
Daniil Naumetc <daniil.naumetc@gmail.com>
2020-12-15 17:07:35 +0100

    added color to todos

--- src/todos.rs
+++ src/todos.rs
@@ -1,14 +1,29 @@
 #[path = "lib/task/mod.rs"] mod task;
 #[path = "lib/storage/mod.rs"] mod storage;
+use colored::*;
 use storage::{FileStorage, Storage};
+use std::string::*;
 
 fn main() {
     let location = std::env::home_dir().map(|path| path.join("todo.json")).expect("No HOME dir found");
     let s = FileStorage::new(location.to_str().unwrap_or("/tmp/todo.json"));
 
-    let task_text = std::env::args().skip(1).collect::<Vec<String>>().join(" ");
+    let tasks = s.get_tasks();
+    if tasks.len() > 0 {
+        let task_text = std::env::args().skip(1).collect::<Vec<String>>().join(" ");
 
-    for (i, task) in s.get_tasks().iter().filter(|t| t.get_body().to_lowercase().contains(&task_text)).enumerate() {
-        println!("{}. {}", i+1, task.get_body());
+        for (i, task) in s.get_tasks().iter().filter(|t| t.get_body().to_lowercase().contains(&task_text)).enumerate() {
+            let mut body = task.get_body().to_string();
+            for hashtag in task.hashtags() {
+                let hashtagged = format!("#{}", hashtag);
+                let hashtagged_colored = hashtagged.yellow().to_string();
+                body = body.replace(&hashtagged, &hashtagged_colored);
+            }
+            println!("{}. {}", i+1, body);
+        }
+    } else {
+        println!("Everything is done!");
     }
+
+    
 }
